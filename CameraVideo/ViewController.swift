866b0768649c00745a2b7a396e392c0042e3dc22
KpStar <kpstar@kpstars-mac.local>
2018-05-07 17:12:43 +0300

    Initial Commit

---
+++ CameraVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CameraVideo
+//
+//  Created by KpStar on 5/7/18.
+//  Copyright © 2018 upwork. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
